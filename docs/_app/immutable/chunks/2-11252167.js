be7d9847b8e395c3548ccaeaff48be869a56a6a5
Jinwoo Lee <woojung3@postech.ac.kr>
2023-01-29 21:44:58 +0900

    draft done

---
+++ docs/_app/immutable/chunks/2-11252167.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-22638994.js";export{t as component};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
-<script>
-    let herbURL = '/html/2023-01-28_rust/index.html'
-</script>
-
 <h1>woojung3's personal page</h1>
 
 <p>
-	Visit <a href="{herbURL}">kit.svelte.dev</a> to read the documentation
+	This page is mainly for updating my presentations for now.
 </p>
+
+<ul>
+	<li><a href="/html/2023-01-28_rust/index.html">Comprehensive Rust (reveal.js 학습용)</a></li>
+</ul>
