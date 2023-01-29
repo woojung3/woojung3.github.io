2155f6b01dcb595df3cb32e570f8e13564e967f4
Jinwoo Lee <woojung3@postech.ac.kr>
2023-01-29 21:37:31 +0900

    add presentation directory

--- docs/_app/immutable/chunks/2-4bc5f119.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-f6a26d5a.js";export{t as component};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
+<script>
+    let herbURL = '/html/2023-01-28_rust/index.html'
+</script>
+
 <h1>woojung3's personal page</h1>
 
 <p>
-	Visit <a href="https://woojung3.github.io/html/2023-01-28 rust">kit.svelte.dev</a> to read the documentation
+	Visit <a href="{herbURL}">kit.svelte.dev</a> to read the documentation
 </p>
