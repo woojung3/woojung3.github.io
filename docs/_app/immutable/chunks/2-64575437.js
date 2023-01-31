3d21ca9a0bed13a273a4fc46fbcf5f4a49f5767e
Jinwoo Lee <woojung3@postech.ac.kr>
2023-01-31 13:10:00 +0900

    update build output

--- docs/_app/immutable/chunks/2-64575437.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.md-9f87859b.js";export{t as component};
---
+++ docs/_app/immutable/chunks/2-88822d29.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.md-9f87a622.js";export{t as component};
