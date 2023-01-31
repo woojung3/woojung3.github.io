3822a3bf271feee50c4f955827d0f13e42650210
Jinwoo Lee <woojung3@postech.ac.kr>
2023-01-31 16:31:41 +0900

    add prism css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 import Header from '$lib/components/Header.svelte'
 import Footer from '$lib/components/Footer.svelte'
 import '$lib/styles/style.scss'
+import '$lib/styles/prism-vsc-dark-plus.css'
 </script>
 
 <Header />
