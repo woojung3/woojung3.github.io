b2a6c823196eb8e48c29b067df6e0fa8a55b8675
Jinwoo Lee <woojung3@postech.ac.kr>
2023-02-02 09:02:23 +0900

    working on width limit (header)

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -14,27 +14,39 @@
     DropdownItem
   } from 'sveltestrap';
 
-  let isOpen = false;
-
-  function handleUpdate(event) {
-    isOpen = event.detail.isOpen;
-  }
+  let currentWidth = 0;
 </script>
+
+<svelte:window bind:outerWidth={currentWidth}/>
+
+<p>size: {currentWidth}px</p>
 
 <Navbar color="light" light expand="md">
   <NavbarBrand href="/">Home</NavbarBrand>
-    <Nav class="ml-auto" navbar>
-      <NavItem>
-        <NavLink href="/presentations">Presentations</NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink href="/papers">Papers</NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink href="/blog">Blog</NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink href="/about">About</NavLink>
-      </NavItem>
-    </Nav>
+    {#if currentWidth >= 800}
+      <Nav class="ml-auto" navbar>
+        <NavItem>
+          <NavLink href="/blog">Blog</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/presentations">Presentations</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/papers">Papers</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/about">About</NavLink>
+        </NavItem>
+      </Nav>
+    {:else}
+      <Dropdown nav inNavbar style="list-style-type: none">
+        <DropdownToggle nav caret>Menu</DropdownToggle>
+        <DropdownMenu right>
+          <DropdownItem href="/blog">Blog</DropdownItem>
+          <DropdownItem href="/presentations">Presentations</DropdownItem>
+          <DropdownItem href="/papers">Papers</DropdownItem>
+          <DropdownItem href="/about">About</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+    {/if}
 </Navbar>
