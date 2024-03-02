f5c2993eba8aff3f47b8cce72ad0b19c6c5eddb5
Jinwoo Lee <woojung3@postech.ac.kr>
2024-03-02 22:00:17 +0900

    add photos and maps to juha-1st-birthday

--- docs/html/juha-first-birthday/scripts/main.js
+++ docs/html/juha-first-birthday/scripts/main.js
@@ -1,5 +1,16 @@
 // Add your javascript here
 // Don't forget to add it into respective layouts where this js file is needed
+$(document).ready(function () {
+    $("#map-image").on("click")
+    {
+
+    }
+
+    $('#go-to-top').click(function () {
+        $('html,body').animate({scrollTop: 0}, 400);
+        return false;
+    });
+})
 
 // Smooth scroll for links with hashes
 $("a.smooth-scroll").click(function (event) {
