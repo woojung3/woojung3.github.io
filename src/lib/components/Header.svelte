9e348081d85e3046c63a3027b6d3c9156137e578
Jinwoo Lee <woojung3@postech.ac.kr>
2025-12-23 00:43:05 +0000

    fix menu

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,6 @@
 <!-- Header.svelte -->
 <script>
+  import { onMount } from 'svelte';
   import {
     Collapse,
     Navbar,
@@ -15,32 +16,39 @@
   } from '@sveltestrap/sveltestrap';
 
   let isOpen = false;
+  let mounted = false;
+
+  onMount(() => {
+    mounted = true;
+  });
 
   function handleUpdate(event) {
     isOpen = event.detail.isOpen;
   }
 </script>
 
-<Navbar class="header" color="light" light expand="md">
-  <NavbarBrand href="/">Home</NavbarBrand>
-  <NavbarToggler on:click={() => (isOpen = !isOpen)} />
-  <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
-    <Nav class="ms-auto" navbar>
-      <NavItem>
-        <NavLink href="/blog">Blog</NavLink>
-      </NavItem>
-      <Dropdown nav inNavbar>
-        <DropdownToggle nav caret>Others</DropdownToggle>
-        <DropdownMenu end>
-          <DropdownItem href="/papers">Papers</DropdownItem>
-          <DropdownItem href="/achievements">Achievements</DropdownItem>
-          <DropdownItem href="/presentations">Presentations</DropdownItem>
-          <DropdownItem divider />
-          <DropdownItem href="/tools">Tools</DropdownItem>
-          <DropdownItem divider />
-          <DropdownItem href="/about">About</DropdownItem>
-        </DropdownMenu>
-      </Dropdown>
-    </Nav>
-  </Collapse>
-</Navbar>
+{#if mounted}
+  <Navbar class="header" color="light" light expand="md">
+    <NavbarBrand href="/">Home</NavbarBrand>
+    <NavbarToggler on:click={() => (isOpen = !isOpen)} />
+    <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+      <Nav class="ms-auto" navbar>
+        <NavItem>
+          <NavLink href="/blog">Blog</NavLink>
+        </NavItem>
+        <Dropdown nav inNavbar>
+          <DropdownToggle nav caret>Others</DropdownToggle>
+          <DropdownMenu end>
+            <DropdownItem href="/papers">Papers</DropdownItem>
+            <DropdownItem href="/achievements">Achievements</DropdownItem>
+            <DropdownItem href="/presentations">Presentations</DropdownItem>
+            <DropdownItem divider />
+            <DropdownItem href="/tools">Tools</DropdownItem>
+            <DropdownItem divider />
+            <DropdownItem href="/about">About</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </Nav>
+    </Collapse>
+  </Navbar>
+{/if}
