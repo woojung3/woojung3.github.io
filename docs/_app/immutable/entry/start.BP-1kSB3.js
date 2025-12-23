5446f640c6a96208ad270be8fcc39220f4b98fff
Jinwoo Lee <woojung3@postech.ac.kr>
2025-12-23 00:36:27 +0000

    fix menu

--- docs/_app/immutable/entry/start.BP-1kSB3.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/Dm8qpGye.js";export{o as load_css,r as start};
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 const config = {
 	plugins: [sveltekit()],
 	ssr: {
-        noExternal: ['@popperjs/core']
+        noExternal: ['@popperjs/core', 'bootstrap']
     }
 };
 
